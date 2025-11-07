10678aa006ea81afdfb5abb5a67025e6035874de
maenTa <shabdalif@gmail.com>
2025-11-07 12:52:30 +0700

    commit

--- app.py
+++ app.py
@@ -9,16 +9,21 @@ race_state = {
     "status": "stopped",  # started, safety_car, yellow_flag, red_flag
     "sector": None,
     "penalties": [],
+    "raceControl": [],
     "safety_active": False
 }
 
-@app.route('/')
+@app.route('/leaderboard')
 def leaderboard():
     return render_template('leaderboard.html')
 
 @app.route('/admin')
 def admin():
     return render_template('admin.html')
+
+@app.route('/')
+def speedometer():
+    return render_template('speedometer.html')
 
 # ----- EVENT SOCKET -----
 @socketio.on('start_lights')
@@ -33,6 +38,21 @@ def handle_race_start():
 @socketio.on('start_formation_lap')
 def handle_formation_lap():
     emit('formation_lap', broadcast=True)
+
+@socketio.on('update_lap')
+def handle_update_lap(data):
+    emit('lap_update', data, broadcast=True)
+
+@socketio.on('update_leaderboard')
+def handle_update_leaderboard(data):
+    # data = [{pos:1, team_logo:'ferrari.png', name:'LEC', gap:'Leader'}, ...]
+    emit('leaderboard_update', data, broadcast=True)
+    
+@socketio.on('session_change')
+def handle_session_change(data):
+    # data = { "session": "RACE" }
+    emit('session_update', data, broadcast=True)
+
 
 
 @socketio.on('flag_event')
@@ -64,6 +84,11 @@ def handle_flag(data):
 def handle_penalty(data):
     race_state["penalties"].append(data)
     emit('penalty_update', data, broadcast=True)
+
+@socketio.on('raceControl')
+def handle_raceControl(data):
+    race_state["raceControl"].append(data)
+    emit('raceControl_update', data, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, port=5002, allow_unsafe_werkzeug=True)
