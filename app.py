3e9a6c73f457e17f4921ca646f9e017cd3026d34
maenTa <shabdalif@gmail.com>
2025-11-07 12:56:37 +0700

    V 1

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit # type: ignore
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'race_secret'
